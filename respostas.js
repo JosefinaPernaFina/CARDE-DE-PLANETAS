1b8dcfbef5d8dfa1f2afb7f143ff1052516ac256
JosefinaPernaFina <00001088855222sp@al.educacao.sp.gov.br>
2024-11-29 14:47:43 -0300

    Add files via upload

---
+++ respostas.js
@@ -0,0 +1,232 @@
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Bimbinha?', 
+  'Eles comunicam-se por troca de luzes e cores.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Prometus?', 
+  'Os alienígenas não dormem, mas entram em um estado de hibernação mental.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Judeail?', 
+  'Eles se alimentam de radiação solar, ao invés de comida.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Julios?', 
+  'Os alienígenas têm uma forma de visão além do espectro visível.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Judas?', 
+  'As espécies alienígenas podem se transformar em diferentes formas de energia.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Rerison?', 
+  'Eles podem viajar no tempo e espaço com o poder da mente.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Gemidos?', 
+  'Os alienígenas se comunicam por campos eletromagnéticos.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Xinco?', 
+  'As espécies vivem em simbiose com plantas inteligentes.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Jalambe?', 
+  'Os alienígenas têm órgãos que se regeneram após a morte.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Bustafar?', 
+  'Eles possuem habilidades telepáticas extremamente poderosas.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Jabucréia?', 
+  'As criaturas alienígenas mudam de cor para expressar emoções.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Jabulane?', 
+  'A alimentação dos alienígenas é feita através da absorção de minerais.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Brazuca?', 
+  'Eles são capazes de modificar a estrutura da matéria à sua vontade.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta oTÁVIO?', 
+  'As espécies alienígenas têm uma visão múltipla com vários olhos.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta 29THD03?', 
+  'Os alienígenas vivem em atmosferas densas de gases pesados.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Sound?', 
+  'Eles comunicam-se através de padrões sonoros que os humanos não conseguem ouvir.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas Junios?', 
+  'Esses seres são capazes de existir sem oxigênio.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas Salo?', 
+  'Alienígenas vivem em planetas com gravidade extremamente baixa.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planea lkjcahiouq?', 
+  'Eles podem viver em ambientes completamente sem luz.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Mocréia?', 
+  'As espécies alienígenas possuem formas de inteligência coletiva.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Bilu?', 
+  'A reprodução alienígena é feita por divisão celular, não por fertilização.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Não?', 
+  'Os alienígenas se comunicam por contato direto com a mente.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Aguento?', 
+  'Essas criaturas podem se alimentar de minerais radioativos.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Mais?', 
+  'As espécies alienígenas possuem uma alimentação baseada em eletricidade.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Mudar?', 
+  'Os alienígenas podem mudar de forma, passando de sólido para gasoso.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Os?', 
+  'Eles podem viver em temperaturas extremas, como o frio absoluto.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Nomes?', 
+  'Essas espécies têm uma expectativa de vida que dura milhares de anos.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Desses?', 
+  'Os alienígenas são capazes de controlar elementos naturais, como o fogo e a água.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Planetas ?', 
+  'Eles vivem em ambientes sem pressão atmosférica.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta AAAAAAA?', 
+  'Essas criaturas podem ter múltiplos cérebros que trabalham em conjunto.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Bitolado?', 
+  'As espécies alienígenas podem se curar instantaneamente de ferimentos graves.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta JuJUba?', 
+  'Eles não possuem órgãos tradicionais como os humanos, mas sim estruturas energéticas.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Nescau?', 
+  'Alienígenas têm a habilidade de mudar sua estrutura física dependendo do ambiente.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta VS?', 
+  'Eles podem gerar campos de força para se protegerem.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Toddy?', 
+  'As espécies alienígenas podem ver em espectros de luz invisíveis para os humanos.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Biscoito?', 
+  'Esses seres alienígenas podem manipular a matéria com a mente.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta OU?', 
+  'Alienígenas se comunicam por pulsos de energia vibracional.'
+)
+
+criaCartao(
+  'Alienígenas e planetas estranhos', 
+  'Quais são os hábitos estranhos das espécies alienígenas do planeta Bolacha?', 
+  'Eles vivem em planetas sem clima estável, onde o tempo muda drasticamente.'
+)
+
+
+
+
+
